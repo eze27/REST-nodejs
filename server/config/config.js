2d3c2c31cfcaae64bba9e3aed28db7933f209a4e
Eze <ezedev1994@gmail.com>
2018-10-20 13:17:43 -0500

    rest server -usuario

--- server/config/config.js
+++ server/config/config.js
@@ -1,3 +1,28 @@
 //puerto
 
 process.env.PORT = process.env.PORT || 3000;
+
+// ===============
+// Entorno de desarrollo (local o produccion)
+// ===============
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+// ===============
+// Entorno de desarrollo MongoDb
+// ===============
+
+let urlDB;
+
+if (process.env.NODE_ENV === 'dev') {
+    //desarrollo local
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    //produccion
+    //pass = aVGBaxDVccSJw8y
+    urlDB = 'mongodb://cafe-user:aVGBaxDVccSJw8y@ds223812.mlab.com:23812/cafe_rest';
+}
+//mongodb://localhost:27017/cafe
+//mongodb://<dbuser>:<dbpassword>@ds223812.mlab.com:23812/cafe_rest
+
+process.env.URLDB = urlDB;
--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 require('./config/config');
 
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 
 const bodyParser = require('body-parser');
@@ -10,41 +11,13 @@ app.use(bodyParser.urlencoded({ extended: false }))
 
 // parse application/json
 app.use(bodyParser.json())
+app.use(require('./routes/usuario'));
 
 
-app.get('/usuario', function(req, res) {
-    res.json('get Usuario LOCAL!!!');
-});
+mongoose.connect(process.env.URLDB, { useNewUrlParser: true }, (err, res) => {
+    if (err) throw err;
 
-app.post('/usuario', function(req, res) {
-
-    let body = req.body;
-
-    if (body.nombre === undefined) {
-
-        res.status(400).json({
-            ok: false,
-            mensaje: 'El nombre es necesario'
-        });
-    } else {
-        res.json({
-            persona: body
-        });
-    }
-
-});
-
-app.put('/usuario/:id', function(req, res) {
-
-    let id = req.params.id;
-
-    res.json({
-        id
-    });
-});
-
-app.delete('/usuario', function(req, res) {
-    res.json('delete Usuario');
+    console.log("conexion establecida");
 });
 
 app.listen(process.env.PORT, () => {
